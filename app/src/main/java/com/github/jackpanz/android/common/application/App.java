117a1430782e69469d1432abf796843ee08e2cc7
panzijie <39301568@qq.com>
2021-02-04 16:53:36 +0800

    添加app类

---
+++ app/src/main/java/com/github/jackpanz/android/common/application/App.java
@@ -0,0 +1,16 @@
+package com.github.jackpanz.android.common.application;
+
+import android.content.Context;
+
+import com.akexorcist.localizationactivity.core.LocalizationApplicationDelegate;
+import com.github.jackpanz.android.common.LocalizationUtils;
+//爱啪啪
+public class App extends android.app.Application{
+
+    private final LocalizationApplicationDelegate localizationDelegate = new LocalizationApplicationDelegate();
+
+    public void attachBaseContext(Context base) {
+        localizationDelegate.setDefaultLanguage(base, LocalizationUtils.getAppDefaultLocale(base));
+        super.attachBaseContext(localizationDelegate.attachBaseContext(base));
+    }
+}
